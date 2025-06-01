dc7f31cd4b39d8825826ed93632824ff287a400e
evalar4 <evelinka-07@mail.ru>
2025-06-01 22:47:56 +0300

    third

--- build/CMakeFiles/state.dir/src/Work.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/state.dir/src/Work.cpp.o: \
- /home/user/evalar4/state/src/Work.cpp /usr/include/stdc-predef.h \
- /home/user/evalar4/state/include/Work.h \
- /home/user/evalar4/state/include/ForenoonState.h \
- /home/user/evalar4/state/include/State.h
